bfb1b614b7d779ab6a2addeb9b68afc37a94e366
Soufiane Raki <soufiane@hackermail.com>
2022-04-16 04:03:27 +0200

    ➕ capturar los datos

--- public/js/agenda.js
+++ public/js/agenda.js
@@ -1,16 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+    // Recolectar los datos del formulario
+    let formulario = document.querySelector('form');
+
     let calendarEl = document.getElementById('agenda');
     let calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
+        //Traducir al español
         locale: "es",
+        //Opciones del calendario
         headerToolbar: {
             left: 'prev,next today',
             center: 'title',
-            right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
         },
         dateClick: function (info) {
             $("#evento").modal("show");
         }
     });
     calendar.render();
+
+    document.getElementById("btnGuardar").addEventListener("click", function () {
+
+        const datos = new FormData(formulario);
+        console.log(datos);
+
+
+    });
 });
